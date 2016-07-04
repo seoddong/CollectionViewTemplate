161bbbc63bbd6ff449667827eb87cd920356f244
SeoDongHee <seoddong@hanmail.net>
2016-07-05 01:00:46 +0900

    First Commit

---
+++ CollectionViewTemplate/UIDesign.swift
@@ -0,0 +1,28 @@
+//
+//  UIDesign.swift
+//  CollectionViewTemplate
+//
+//  Created by SeoDongHee on 2016. 7. 5..
+//  Copyright © 2016년 SeoDongHee. All rights reserved.
+//
+
+import UIKit
+
+class UIDesign {
+    
+    func setTextFieldLayout(textField: UITextField, fontsize: CGFloat) {
+        textField.textAlignment = .Center
+        textField.font = UIFont(name: "Verdana", size: fontsize)
+        textField.layer.borderColor = UIColor.blackColor().CGColor
+        textField.layer.borderWidth = 3
+        textField.layer.cornerRadius = 10
+        textField.backgroundColor = UIColor.lightGrayColor()
+    }
+    
+    func setLabelLayout(label: UILabel, fontsize: CGFloat) {
+        label.textAlignment = .Center
+        label.font = UIFont(name: "Verdana", size: fontsize)
+        label.layer.cornerRadius = 10
+
+    }
+}
--- SettingsViewController.swift
+++ SettingsViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class ImageViewCell: UICollectionViewCell {
-    var imageView: UIImageView?
-    var cellLabel: UILabel?
-    var textField: UITextField?
+class SupplementaryView: UICollectionReusableView {
+    var imageView = UIImageView()
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        self.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 
+class SettingsCell: UICollectionViewCell {
+    var imageView = UIImageView()
+    var cellLabel = UILabel()
+    var textField = UITextField()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.contentView.addSubview(cellLabel)
+        cellLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.contentView.addSubview(textField)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        self.contentView.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -24,12 +45,17 @@ class ImageViewCell: UICollectionViewCell {
 
 class SettingsViewController: UIViewController {
     
+    let uidesign = UIDesign()
     let layout = UICollectionViewFlowLayout()
     var collectionView: UICollectionView!
     
     let reuseIdentifier = "reuseIdentifier"
+    let headerIdentifier = "headerIdentifier"
+    let sections = ["image", "image"]
     let cells = ["label", "textField", "image", "label", "textField", "image", "label", "textField", "image", "label", "textField", "image"]
 
+    var collectionViewWidth: CGFloat = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -48,17 +74,27 @@ class SettingsViewController: UIViewController {
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "이전 단계로", style: .Plain, target: self, action: #selector(self.leftBarButtonPressed))
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "다음 단계로", style: .Plain, target: self, action: #selector(self.nextButtonPressed))
         
+        layout.scrollDirection = .Vertical
+        layout.headerReferenceSize = (UIImage(named: "name")?.size)!
+        layout.sectionHeadersPinToVisibleBounds = true
+        layout.minimumLineSpacing = 10
+        layout.minimumInteritemSpacing = 0
+        layout.sectionInset = UIEdgeInsetsZero
+        
         collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
         collectionView.backgroundColor = UIColor.yellowColor()
-        collectionView.collectionViewLayout = UICollectionViewFlowLayout()
+        collectionView.collectionViewLayout = layout
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(collectionView)
-        collectionView.registerClass(ImageViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        collectionView.registerClass(SettingsCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        collectionView.registerClass(SupplementaryView.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: headerIdentifier)
+        
+        collectionViewWidth = collectionView.frame.size.width
         
         let viewsDictionary = ["collectionView": collectionView]
-        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-20-[collectionView]-20-|",
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[collectionView]|",
             options: .AlignAllCenterX, metrics: nil, views: viewsDictionary))
-        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-\((self.navigationController?.navigationBar.frame.size.height)! + 40)-[collectionView]-20-|",
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[collectionView]|",
             options: .AlignAllCenterY, metrics: nil, views: viewsDictionary))
         
     }
@@ -102,7 +138,7 @@ extension SettingsViewController: UITextFieldDelegate {
 
 extension SettingsViewController: UICollectionViewDataSource {
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-        return 1
+        return sections.count
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -110,65 +146,104 @@ extension SettingsViewController: UICollectionViewDataSource {
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! ImageViewCell
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! SettingsCell
+
         switch cells[indexPath.row] {
         case "label":
-            if let label = cell.cellLabel {
-                label.text = "nnname"
-                label.hidden = false
-            }
-            else {
-                
-                cell.cellLabel = UILabel(frame: cell.frame)
-                cell.addSubview(cell.cellLabel!)
-                cell.cellLabel?.hidden = false
-                cell.cellLabel?.text = "name"
-            }
-            cell.textField?.hidden = true
-            cell.imageView?.hidden = true
+
+            //cell.cellLabel.hidden = false
+            cell.cellLabel.frame = cell.contentView.frame
+            cell.cellLabel.text = "이름을 적어주세요.\(indexPath.row)"
+            uidesign.setLabelLayout(cell.cellLabel, fontsize: 40)
+
             break
+            
         case "textField":
-            if let tf = cell.textField {
-                tf.text = "008"
-                tf.hidden = false
-            }
-            else {
-                
-                cell.textField = UITextField(frame: cell.frame)
-                cell.addSubview(cell.textField!)
-                cell.textField?.hidden = false
-                cell.textField?.text = "007"
-            }
-            cell.cellLabel?.hidden = true
-            cell.imageView?.hidden = true
+
+            cell.textField.frame = cell.contentView.frame
+            cell.textField.text = "007_\(indexPath.row)"
+            uidesign.setTextFieldLayout(cell.textField, fontsize: 40)
+
             break
+            
         case "image":
-            if let img = cell.imageView {
-                img.hidden = false
-            }
-            else {
-                
-                cell.imageView = UIImageView(image: UIImage(named: "next"))
-                cell.addSubview(cell.imageView!)
-                cell.imageView?.hidden = false
-            }
-            cell.textField?.hidden = true
-            cell.cellLabel?.hidden = true
+
+            cell.imageView.frame = cell.contentView.frame
+            cell.imageView.image = UIImage(named: "next")
+            cell.imageView.contentMode = .ScaleAspectFit
+
             break
+            
         default:
+            debugPrint("default")
             break
         }
         return cell
     }
     
+    // 섹션 헤더 설정
+    func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
+        var headerView: SupplementaryView?
+        if (kind == UICollectionElementKindSectionHeader) {
+            headerView = collectionView.dequeueReusableSupplementaryViewOfKind(kind, withReuseIdentifier: headerIdentifier, forIndexPath: indexPath) as? SupplementaryView
+            let image = indexPath.row % 2 == 0 ? UIImage(named: "name") : UIImage(named: "age")
+            headerView?.imageView.image = image
+            headerView?.imageView.contentMode = .ScaleAspectFit
+            headerView?.imageView.frame = CGRectMake(0, 0, collectionViewWidth, image!.size.height)
+            headerView?.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.5)
+        }
+        return headerView!
+    }
+    
     func collectionView(collectionView: UICollectionView, willDisplayCell cell: UICollectionViewCell, forItemAtIndexPath indexPath: NSIndexPath) {
-        cell.backgroundColor = UIColor.greenColor()
+        let cell = cell as! SettingsCell
+        
+        switch cells[indexPath.row] {
+        case "label":
+            cell.backgroundColor = UIColor(red: 0, green: 1, blue: 0, alpha: 0.5)
+            cell.cellLabel.hidden = false
+            cell.imageView.hidden = true
+            cell.textField.hidden = true
+            break
+            
+        case "textField":
+            cell.backgroundColor = UIColor(red: 1, green: 0.5, blue: 0, alpha: 0.5)
+            cell.textField.hidden = false
+            cell.cellLabel.hidden = true
+            cell.imageView.hidden = true
+            break
+            
+        case "image":
+            cell.backgroundColor = UIColor(red: 0, green: 0.5, blue: 1, alpha: 0.5)
+            cell.imageView.hidden = false
+            cell.cellLabel.hidden = true
+            cell.textField.hidden = true
+            break
+            
+        default:
+            break
+            
+        }
     }
 }
 
 extension SettingsViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
         
-        return CGSizeMake(400, 200)
+        switch cells[indexPath.row] {
+        case "label":
+            return CGSizeMake(collectionViewWidth, 200)
+
+        case "image":
+            let img = UIImage(named: "next")
+            return CGSizeMake(collectionViewWidth, 200) //(img?.size.height)!)
+
+        case "textField":
+            return CGSizeMake(collectionViewWidth-200, 100)
+            
+        default:
+            return CGSizeMake(collectionViewWidth, 200)
+
+        }
     }
 }
